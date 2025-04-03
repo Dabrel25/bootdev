02537b34f1c447158ca8bd6015cc1994cee5cf61
Darrel Ong <darrelong@Darrels-MacBook-Air.local>
2025-04-03 20:49:19 +0800

    Added wordcounter

--- BookBot/main.py
+++ BookBot/main.py
@@ -1,12 +1,15 @@
+from stats import get_book_wordnum
 import os 
 
 def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
-    print(text)
+    length = get_book_wordnum(text)
+    print(f"{length} words found in the document")
 
 def get_book_text(filepath):
     with open(filepath) as f:
-        return f.read()
+        book_content = f.read()
+        return book_content
     
 main()
---
+++ BookBot/stats.py
@@ -0,0 +1,6 @@
+
+
+def get_book_wordnum(book_content):
+    split_book_content = book_content.split()
+    return len(split_book_content)
+    
