8fd26c4ccbd2fae332662f887ff2719759b3ae9a
Darrel Ong <darrelong@Darrels-MacBook-Air.local>
2025-04-03 22:31:39 +0800

    created formatting and sorted character count

--- BookBot/main.py
+++ BookBot/main.py
@@ -1,15 +1,33 @@
 from stats import get_book_wordnum
+from stats import get_book_characternum
+from stats import sort_charcounts
 import os 
 
 def main():
     filepath = "books/frankenstein.txt"
     text = get_book_text(filepath)
     length = get_book_wordnum(text)
-    print(f"{length} words found in the document")
+    char_counts = get_book_characternum(text)
+    sorted_chars = sort_charcounts(char_counts)
+    format_textreport(filepath,length, sorted_chars)
 
 def get_book_text(filepath):
     with open(filepath) as f:
         book_content = f.read()
         return book_content
-    
+
+def format_textreport(filepath, length, sorted_chars):
+    print(f"""
+============ BOOKBOT ============ 
+Analyzing book found at {filepath}...
+----------- Word Count ----------
+Found {length} total words
+--------- Character Count -------""")
+    for char_dict in sorted_chars:
+        char = char_dict["char"]
+        count = char_dict["count"]
+        if char.isalpha():
+            print(f"{char}: {count}")
+
+    print("============= END ===============")
 main()
--- BookBot/stats.py
+++ BookBot/stats.py
@@ -1,6 +1,24 @@
-
+from collections import Counter
 
 def get_book_wordnum(book_content):
     split_book_content = book_content.split()
     return len(split_book_content)
+
+def get_book_characternum(book_content):
+    lower_book_content = book_content.lower()
+    list_book_char = list(lower_book_content)
+    char_counts = Counter(list_book_char)
+    return char_counts
+
+def sort_charcounts(char_counts):
+
+    char_list = []
+    for char, count in char_counts.items():
+        char_list.append({"char": char, "count": count})
+        
+    def sort_on(dict):
+        return dict["count"]
     
+    char_list.sort(reverse=True, key=sort_on)
+    
+    return char_list
