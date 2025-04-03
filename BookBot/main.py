5d6ec75fa6dd0f5732cb5edcada94adba8cd4d69
Darrel Ong <darrelong@Darrels-MacBook-Air.local>
2025-04-03 18:38:09 +0800

    main.py now prints frankenstein.txt to the terminal

--- BookBot/main.py
+++ BookBot/main.py
@@ -1,2 +1,12 @@
-print('Ready for bookbot')
-print("greetings boots")
+import os 
+
+def main():
+    filepath = "books/frankenstein.txt"
+    text = get_book_text(filepath)
+    print(text)
+
+def get_book_text(filepath):
+    with open(filepath) as f:
+        return f.read()
+    
+main()
